a4349a8eca0a5b1da91cca8c618a19b177ae56af
명빈 한 <hmbgaq1@gmail.com>
2023-07-21 20:54:09 +0900

    230721 2054

--- 230608/MyungBin/Lobby.h
+++ 230608/MyungBin/Lobby.h
@@ -4,9 +4,9 @@
 class Lobby : public MiniGame
 {
 public:
-	virtual void Initialize() override;
-	virtual void Update() override;
-	virtual void Late_Update() override;
-	virtual void Render(HDC hDC) override;
-	virtual void Release() override;
+	virtual void				Initialize() override;
+	virtual MyungBinStageState	Update() override;
+	virtual void				Late_Update() override;
+	virtual void				Render(HDC hDC) override;
+	virtual void				Release() override;
 };
